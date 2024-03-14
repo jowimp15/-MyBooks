7fca7d560e5b25816a0452531a482efe9b91b4e0
jowi <jowimp15@gmail.com>
2024-03-14 18:59:47 -0300

    traying to repair errors #2

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import ReactDOM from 'react-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+  //   <React.StrictMode>
+  //   </React.StrictMode>,
+  // )
+
+
+import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+
+
+const domNode = document.getElementById('root');
+if(domNode){
+
+  const root = createRoot(domNode);
+  root.render(<App />)
+
+}
+
