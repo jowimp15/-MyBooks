5d0eeba84238924d12bca9038741fed6fcc71460
jowi <jowimp15@gmail.com>
2024-08-05 11:35:04 -0300

    update url base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://jowimp15.github.io/mybooks.github.io",
+  base:"https://jowimp15.github.io/mybooks",
   css: {
     preprocessorOptions: {
       sass: {
